99b36dad58d6a141424a3f5831f205644d44ba9e
hdbrooks1 <114303747+hdbrooks1@users.noreply.github.com>
2023-07-07 19:09:12 -0500

    final_v2
    It has the stuff for menu options 1 and 2.

---
+++ Module_11/mysql_final_v2.py
@@ -0,0 +1,121 @@
+import mysql.connector
+from mysql.connector import errorcode
+
+config = {
+    "user": "whatabook_user",
+    "password": "MySQL8IsGreat!",
+    "host": "127.0.0.1",
+    "database": "whatabook",
+    "raise_on_warnings": True
+}
+
+try:
+    db = mysql.connector.connect(**config)
+    
+    print("\nDatabase user {} connected to MySQL on host {} with database {}".format(config["user"], config["host"], config["database"]))
+    
+    #input("\n\n Press any key to continue...")
+    
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("The supplied username or password are invalid")
+        
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("The specified database does not exist")
+        
+    else: 
+        print(err)
+        
+#The cursor being defined
+cursor = db.cursor()
+
+
+#Defining the main menu
+def show_menu():
+    print("\nMenu:")
+    print("1. View Books")
+    print("2. View Store Locations")
+    print("3. My Account")
+    print("4. Exit Program")
+    
+    
+#method for show books | Should work on a cleaner output? But how?
+def show_books(cursor):
+    cursor.execute("SELECT book_id, book_name, author, details FROM book;")
+    books = cursor.fetchall()
+    
+    print("\nHere are the books that we have!:")
+    for book in books:
+        book_id, book_name, author, details = book
+        print("Book ID: {}, Book Name: {}, Author: {}, Details: {}".format(book_id, book_name, author, details))
+        
+        
+#method for show locations | I think this looks right for the output?
+def show_locations(cursor):
+    cursor.execute("SELECT store_id, locale FROM store;")
+    locations = cursor.fetchall()
+    
+    print("\nOur Store Locations!:")
+    for location in locations:
+        store_id, locale = location
+        print("Store ID: {}, Location Address: {}".format(store_id, locale))
+
+#method for validate user
+
+
+#method for show account menu
+def show_account_menu():
+    print("\nAccount Menu:")
+    print("1. Wishlist")
+    print("2. Add Book")
+    print("3. Main Menu")
+
+#method for show wishlist
+
+
+#method for show books to add
+
+
+#method for add book to wishlist
+
+
+
+
+#Main program!
+while True:
+    show_menu()
+    pick = input("\nPlease pick a number (1-4): ")
+
+    
+    if pick == '1':
+        show_books(cursor)
+    elif pick == '2':
+        show_locations(cursor)
+    elif pick == '3':
+        #Please don't forget to validate the user before this
+        
+        #After user is validated
+        while True:
+            show_account_menu()
+            account_pick = input("\n Please pick a number (1-3): ")
+            
+            if account_pick == '1':
+                print("\nThis is 1 where it shows stuff")
+            elif account_pick == '2':
+                print("\nShow stuff for 2 here")
+            elif account_pick == '3':
+                break
+            else:
+                print("Your choice is invalid. Please try again.")
+        
+        
+        
+    elif pick == '4':
+        break
+    else:
+        print("Your choice is invalid. Please try again.")
+    
+
+#Close the connection and cursor
+cursor.close()
+db.close()
